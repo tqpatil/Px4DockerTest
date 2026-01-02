5a53a80efd55c45b710b652fd02db2cda3507cd4
Tanishq Patil <trpatil@Tanishqs-MacBook-Pro.local>
2026-01-02 00:16:31 -0800

    fix

--- ci/run_px4_ros2.sh
+++ ci/run_px4_ros2.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 echo "PX4 + ROS 2 TEST"
 export PX4_SIM_SPEED_FACTOR=2
 export ROS_DOMAIN_ID=0
-export PX4_GZ_MODEL=x500
+# export PX4_GZ_MODEL=x500
 export HEADLESS=1
 PX4_DIR=~/workspace/PX4-Autopilot
 ROS_WS=~/workspace/ros2_ws
@@ -11,7 +11,7 @@ ROS_WS=~/workspace/ros2_ws
 # build PX4
 echo "Building PX4..."
 cd "$PX4_DIR"
-make px4_sitl
+make px4_sitl gz_x500
 #build ROS
 echo "Building ROS..."
 source /opt/ros/humble/setup.bash
@@ -24,7 +24,7 @@ MicroXRCEAgent udp4 -p 8888 &
 DDS_PID=$!
 echo "Starting PX4 Simulation..."
 cd "$PX4_DIR"
-make px4_sitl &
+make px4_sitl gz_x500 &
 PX4_PID=$!
 #debug
 sleep 20
