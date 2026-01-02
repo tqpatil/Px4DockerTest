ab2e7fd86063d48a9eb4617a46b8b30da597a940
Tanishq Patil <trpatil@Tanishqs-MacBook-Pro.local>
2026-01-02 00:28:36 -0800

    fix

--- ci/run_px4_ros2.sh
+++ ci/run_px4_ros2.sh
@@ -5,6 +5,8 @@ export PX4_SIM_SPEED_FACTOR=2
 export ROS_DOMAIN_ID=0
 # export PX4_GZ_MODEL=x500
 export HEADLESS=1
+export QT_QPA_PLATFORM=offscreen
+
 PX4_DIR=~/workspace/PX4-Autopilot
 ROS_WS=~/workspace/ros2_ws
 
@@ -22,15 +24,22 @@ source install/setup.bash
 echo "Starting microxrce agent..."
 MicroXRCEAgent udp4 -p 8888 &
 DDS_PID=$!
+#start PX4 Simulation
 echo "Starting PX4 Simulation..."
 cd "$PX4_DIR"
+source /opt/ros/humble/setup.bash
+source "$ROS_WS/install/setup.bash"
 make px4_sitl gz_x500 &
 PX4_PID=$!
 #debug
-sleep 20
-ros2 topic list | tee /tmp/topics.txt
-grep -q "/fmu/out/sensor_combined" /tmp/topics.txt
+timeout 60 bash -c '
+until ros2 topic list | tee /tmp/topics.txt | grep -q "/fmu/out/sensor_combined"; do
+  sleep 2
+done
+'
+
 timeout 10 ros2 topic echo /fmu/out/sensor_combined --once
 echo "Cleaning up..."
-kill $PX4_PID $DDS_PID || true
+kill -- -$$ || true
+
 echo "Basic PX4 + ROS 2 test ran succesfully"
